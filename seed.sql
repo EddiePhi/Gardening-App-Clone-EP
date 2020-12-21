61cc0bcfe74e2311d93af8f12592e7a09c04ebe8
Eddie Phimansone <ephimansone@gmail.com>
2020-12-21 16:18:30 -0500

    populated seed.sql to test in MySQL, test success

---
+++ seed.sql
@@ -0,0 +1,17 @@
+-- This is used as a quick reference to run in MySQL to created our database.
+
+DROP DATABASE IF EXISTS gardening_DB;
+CREATE database gardening_DB;
+
+USE gardening_DB;
+
+CREATE TABLE plants (
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  plant_name VARCHAR(30) NOT NULL,
+  price DECIMAL(10, 2) NOT NULL,
+  seed_ct INT (10) NOT NULL
+);
+
+INSERT INTO plants (plant_name, price, seed_ct)
+VALUES ("Tomato", 4.99, 100), ("Spinach", 3.99, 100);
+
