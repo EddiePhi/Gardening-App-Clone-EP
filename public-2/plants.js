172361ee96c989ee0bf66c68771f7ba5dfe87ca9
Danielle Steinman <daniellesteinman@Danielles-MacBook-Air.local>
2021-01-15 19:51:09 -0500

    added onclick to buttons on separate js page

---
+++ public-2/plants.js
@@ -0,0 +1,47 @@
+var tomatoButton = document.getElementById("tomato");
+var lettuceButton = document.getElementById("lettuce");
+var spinachButton = document.getElementById("spinach");
+var cucumberButton = document.getElementById("cucumber");
+var peppersButton = document.getElementById("peppers");
+var thymeButton = document.getElementById("thyme");
+var dillButton = document.getElementById("dill");
+var sageButton = document.getElementById("sage");
+var rosemaryButton = document.getElementById("rosemary");
+
+
+
+tomatoButton.onclick = function() {
+    alert("clicked tomato")
+};
+
+lettuceButton.onclick = function() {
+    alert("clicked lettuce")
+};
+
+spinachButton.onclick = function() {
+    alert("clicked spinach")
+}
+
+cucumberButton.onclick = function() {
+    alert("clicked cucumber")
+};
+
+peppersButton.onclick = function() {
+    alert("clicked peppers")
+};
+
+thymeButton.onclick = function() {
+    alert("clicked thyme")
+};
+
+dillButton.onclick = function() {
+    alert("clicked dill")
+}
+
+sageButton.onclick = function() {
+    alert("clicked sage")
+};
+
+rosemaryButton.onclick = function() {
+    alert("clicked rosemary")
+}
