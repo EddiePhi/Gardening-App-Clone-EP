57514c92ff8ea22e2df1a2adc35afeb2e4184f37
Danielle Steinman <daniellesteinman@Danielles-MacBook-Air.local>
2021-01-19 18:40:14 -0500

    final edit before class

--- public/js/plants.js
+++ public/js/plants.js
@@ -1,9 +1,27 @@
 //make a fetch call to the API, save the data
-fetch("http://localhost:8080/api/plants"
-).then(response => response.json())
-.then(plantdata => {
-  console.log(plantdata);
-});
+fetch("http://localhost:8080/api/plants")
+  .then((response) => response.json())
+  .then((plantdata) => {
+    //console log the results
+    console.log(plantdata);
+    //console log the results with parameters
+    console.log(plantdata[0].plant_facts);
+
+    //narrow parameters further
+    for (let i = 0; i < plantdata.length; i++) {
+      // create variables
+      let plantFacts = plantdata[i].plant_facts;
+      let plantName = plantdata[i].plant_name;
+      let daysToMaturity = plantdata[i].days_to_maturity;
+      let sun = plantdata[i].sun;
+
+      // test variables
+      // console.log(plantName);
+      console.log(plantFacts);
+      // console.log(daysToMaturity)
+      console.log(`${plantName} is a plant for your garden and it takes ${daysToMaturity} days to mature and it needs ${sun}`);
+    }
+  });
 
 // Buttons Variables
 const tomBtn = document.getElementById("tomato");
@@ -29,34 +47,33 @@ tomBtn.onclick = () => {
 };
 letBtn.onclick = () => {
   modal.style.display = "block";
-}
+};
 spinBtn.onclick = () => {
   modal.style.display = "block";
-}
+};
 cucBtn.onclick = () => {
   modal.style.display = "block";
-}
+};
 pepBtn.onclick = () => {
   modal.style.display = "block";
-}
+};
 thyBtn.onclick = () => {
   modal.style.display = "block";
-}
+};
 dillBtn.onclick = () => {
   modal.style.display = "block";
-}
+};
 sageBtn.onclick = () => {
   modal.style.display = "block";
-}
+};
 roseBtn.onclick = () => {
   modal.style.display = "block";
-}
+};
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = () => {
   modal.style.display = "none";
-}
-
+};
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = (event) => {
