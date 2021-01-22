1ad2dc7da06fac05d9caa6faf4b25146da2a566c
Danielle Steinman <daniellesteinman@Danielles-MacBook-Air.local>
2021-01-21 22:03:54 -0500

    polished it, incorporated date and time. It should be done question mark

--- public/js/plants.js
+++ public/js/plants.js
@@ -45,10 +45,10 @@ fetch("/api/plants")
 });
 
 //maybe change plantdata to plants at some point
-function generatePlantCards (plants) {
-  for (let i = 0; i < plants.length; i++) {
+function generatePlantCards (plantdata) {
+  for (let i = 0; i < plantdata.length; i++) {
     // create variables
-    const {plant_name, plant_facts, days_to_maturity, fruit_size_inches, sun, spread, height} = plants[i];
+    const {plant_name, plant_facts, days_to_maturity, fruit_size_inches, sun, spread, height} = plantdata[i];
 
     const plantButton = `<button class="mx-6 my-6 nes-container has-background-white plant-button is-rounded"
     id="${plant_name}"
@@ -68,116 +68,15 @@ function generatePlantCards (plants) {
   };
 };
 
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = (event) => {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 };
-
-// //make a fetch call to the API, save the data
-// fetch("/api/plants")
-//   .then((response) => response.json())
-//   .then((plantdata) => {
-//     //console log the results
-//     console.log(plantdata);
-//     //console log the results with parameters
-//     //narrow parameters further
-//     for (let i = 0; i < plantdata.length; i++) {
-//       // create variables
-//       let id = plantdata[i].id;
-//       let plantName = plantdata[i].plant_name;
-//       let plantFacts = plantdata[i].plant_facts;
-//       let daysToMaturity = plantdata[i].days_to_maturity;
-//       let fruitSize = plantdata[i].fruit_size_inches;
-//       let sun = plantdata[i].sun;
-//       let spread = plantdata[i].spread;
-//       let height = plantdata[i].height;
-
-//       console.log(`${plantName} is a plant for your garden. ${plantFacts} ${plantName} takes ${daysToMaturity} days to mature and it needs ${sun}. Its fruit can grow to be ${fruitSize} inches. It can grow to be ${spread} inches wide and ${height} inches tall.`);
-
-//     };
-//   });
-
-
-
-  // <!-- MODAL TO BE DYNAMICALLY CREATED -->
-  // <div class="modal" id="modal">
-  //   <div class="modal-content">
-  //     <span class="close">&times;</span>
-  //     <h1>(Description description description description)</h1>
-  //     <p>Sun: </p>
-  //     <br>
-  //     <p>Life cycle:</p>
-  //     <br>
-  //     <p>Spread:</p>
-  //     <br>
-  //     <p>Days to maturity:</p>
-  //     <br>
-  //     <p>Height:</p>
-  //     <br>
-  //     <p>Sow Method:</p>
-  // </div>
-  // </div>
-
-// // Buttons Variables
-// const tomBtn = document.getElementById("tomato");
-// const letBtn = document.getElementById("lettuce");
-// const spinBtn = document.getElementById("spinach");
-// const cucBtn = document.getElementById("cucumber");
-// const pepBtn = document.getElementById("peppers");
-// const thyBtn = document.getElementById("thyme");
-// const dillBtn = document.getElementById("dill");
-// const sageBtn = document.getElementById("sage");
-// const roseBtn = document.getElementById("rosemary");
-
-// // Copied and pasted from w3 schools
-// // Get the Modal
-// // const modal = document.getElementById("modal");
-
-// // Get the <span> element that closes the modal
-// const span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks the button, open the modal
-// tomBtn.onclick = () => {
-//   modal.style.display = "block";
-// };
-// letBtn.onclick = () => {
-//   modal.style.display = "block";
-// };
-// spinBtn.onclick = () => {
-//   modal.style.display = "block";
-// };
-// cucBtn.onclick = () => {
-//   modal.style.display = "block";
-// };
-// pepBtn.onclick = () => {
-//   modal.style.display = "block";
-// };
-// thyBtn.onclick = () => {
-//   modal.style.display = "block";
-// };
-// dillBtn.onclick = () => {
-//   modal.style.display = "block";
-// };
-// sageBtn.onclick = () => {
-//   modal.style.display = "block";
-// };
-// roseBtn.onclick = () => {
-//   modal.style.display = "block";
-// };
-
-// When the user clicks on <span> (x), close the modal
-// span.onclick = () => {
-//   modal.style.display = "none";
-// };
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = (event) => {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// };
 
 
 // Weather icon GET request
