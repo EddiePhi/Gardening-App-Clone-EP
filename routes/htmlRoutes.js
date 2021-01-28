cbc9a251fb6cdad8f29d4fe66ecfa9698bc61e77
Eddie Phimansone <ephimansone@gmail.com>
2021-01-28 18:16:07 -0500

    signup and login pages work and save to database

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,6 +3,9 @@
 // ===============================================================================
 const path = require("path");
 
+// Requiring our custom middleware for checking if a user is logged in (Wk-14 HW)
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
 // ===============================================================================
 // ROUTING
 // ===============================================================================
@@ -16,15 +19,23 @@ module.exports = function (app) {
 
   // Custom html routes
   app.get("/", function (req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/home");
+    }
     res.sendFile(path.join(__dirname, "../public/html/landingpage.html"));
   });
+
+
 
   app.get("/plants", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/html/plants.html"));
   });
 
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
   // Route to landing page
-  app.get("/home", function (req, res) {
+  app.get("/home", isAuthenticated, function (req, res) {
     res.sendFile(path.join(__dirname, "../public/html/plotHomePage.html"));
   });
   // Route to forecast page
@@ -37,15 +48,39 @@ module.exports = function (app) {
     res.sendFile(path.join(__dirname, "../public/html/plotpage.html"));
   });
 
-  // If no matching route is found default to landing page/home page
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/html/landingpage.html"));
-  });
-
-
-
+ 
 
   // Routes added for P3, WIP (EP)
+
+  // Route to the Login page
+  app.get("/login", function (req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/home");
+    }
+    res.sendFile(path.join(__dirname, "../public/html/login.html"));
+  });
+
+  // Route for logging user out
+  app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+  });
+  
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get("/members", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/members.html"));
+  });
+  
+
+  // Route to the Signup page
+  app.get("/signup", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/html/signup.html"));
+  });
+
+  
+
   // Route to the Community page
   app.get("/community", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/html/community.html"));
@@ -79,6 +114,14 @@ module.exports = function (app) {
   // Route to the Plant recommendations page
   app.get("/what-to-grow", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/html/what-to-grow.html"));
+  });
+
+
+
+  
+   // If no matching route is found default to landing page/home page
+   app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/html/landingpage.html"));
   });
 
 };
